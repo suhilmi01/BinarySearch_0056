824dbe97b11bf27846ac3d5c14ce941c8cc25735
suhilmi01 <suhilmi01@mail.com>
2026-04-16 18:10:00 +0700

    membuat fungsi input

--- BinarySearch.cpp
+++ BinarySearch.cpp
@@ -2,6 +2,30 @@
 using namespace std;
 
 //deklarasi variabel global
-int elemen[10];
+int element[10];
 int nPanjang;
 int x;
+
+void input()
+{
+    while (true)
+    {
+        cout << "masukkan banyaknya element pada array (maksimal 10): ";
+        cin >> nPanjang;
+
+
+        if (nPanjang <= 10)
+        {
+            break;
+        }
+        else
+        {
+            cout << "\n[!] jumlah element tidak boleh lebih dari 10. silahkan coba lagi.\n";
+        }
+    }
+
+    cout << "\n=============================\n";
+    cout << "      masukkan elemen array    \n";
+    cout << "===============================\n";
+
+    
