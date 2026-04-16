71b317b0caefad676e5a30a7ed89a9985b5f5ef9
suhilmi01 <suhilmi01@mail.com>
2026-04-16 18:14:52 +0700

    membuat perulangan untuk menampilkan data

--- BinarySearch.cpp
+++ BinarySearch.cpp
@@ -51,4 +51,20 @@ void bubblesortArray()
     }while ( pass <= nPanjang - 1);    
 }
 void display()
+{
+    cout << "\n===================================================\n";
+    cout << "\n      Elemen Array Setelah Diurutkan (Asc)         \n";
+    cout << "\n===================================================\n";
+
+    for (int j = 0; j < nPanjang; j++)
+    {
+        cout << element[j];
+        if (j < nPanjang - 1)
+        {
+            cout << " -> ";
+        }
+    }
+    cout << endl;
+}
+
     
