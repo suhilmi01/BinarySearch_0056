a4d6ca76988e37aee0cf9c86f45c458515812896
suhilmi01 <suhilmi01@mail.com>
2026-04-16 18:13:16 +0700

    membuat perulangan do while untu melakukan bubblesort

--- BinarySearch.cpp
+++ BinarySearch.cpp
@@ -34,4 +34,20 @@ void input()
     }
 }
 void bubblesortArray()
+{
+    int pass = 1;
+    do
+    {
+        for (int j = 0; j <= nPanjang -1 -pass; j++)
+        {
+            if (element[j] > element[j + 1])
+            {
+                int temp = element[j];
+                element[j] = element[j + 1];
+                element[j +1] = temp;
+            }
+        }    
+        pass++;
+    }while ( pass <= nPanjang - 1);    
+}
     
